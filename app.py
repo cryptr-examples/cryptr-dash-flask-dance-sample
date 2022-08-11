cf1970bb5cd53bd0687801212a3a046f06fc2a97
Thibaud RENAUX <thibaud.renaux@gmail.com>
2022-08-11 15:14:25 +0200

    setup a simple multi-page app

--- app.py
+++ app.py
@@ -1,51 +1,82 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
+import dash
 
 from dash import Dash, dcc, html, Output, Input, State
-import plotly.express as px
-import pandas as pd
+# CREDIT: This code is copied from Dash official documentation:
+# https://dash.plotly.com/urls
 
-app = Dash(__name__)
+# Since we're adding callbacks to elements that don't exist in the app.layout,
+# Dash will raise an exception to warn us that we might be
+# doing something wrong.
+# In this case, we're adding the elements through a callback, so we can ignore
+# the exception.
+app = Dash(__name__, suppress_callback_exceptions=True)
 
-# assume you have a "long-form" data frame
-# see https://plotly.com/python/px-arguments/ for more options
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-    html.Div(dcc.Input(id='input-on-submit', type='text')),
-    html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Div(id='container-button-basic',
-             children='Enter a value and press submit'),
-
-    html.Div(children='''
-        Dash: A web application framework for your data.
-    '''),
-
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
 ])
 
-@app.callback(
-  Output('container-button-basic', 'children'),
-  Input('submit-val', 'n_clicks'),
-  State('input-on-submit', 'value'),
-)
 
-def update_output(n_clicks, value):
-  return 'The input value was "{}" and the button has been clicked {} times'.format(
-    value,
-    n_clicks
-  )
+index_page = html.Div([
+    dcc.Link('Go to Secured Page', href='/secured-page'),
+    html.Br(),
+    dcc.Link('Go to login', href='/login'),
+])
+
+secured_layout = html.Div([
+    html.H1('Secured Page'),
+    dcc.Dropdown(
+        id='page-1-dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='page-1-content'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/'),
+])
+
+
+@app.callback(Output('page-1-content', 'children'),
+              [Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return 'You have selected "{}"'.format(value)
+
+
+login_layout = html.Div([
+    html.H1('Login Page'),
+    dcc.RadioItems(
+        id='page-2-radios',
+        options=[{'label': i, 'value': i} for i in ['Orange', 'Blue', 'Red']],
+        value='Orange'
+    ),
+    html.Div(id='page-2-content'),
+    html.Br(),
+    dcc.Link('Go to Page 1', href='/page-1'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/')
+])
+
+
+@app.callback(Output('page-2-content', 'children'),
+              [Input('page-2-radios', 'value')])
+def page_2_radios(value):
+    return 'You have selected "{}"'.format(value)
+
+
+# Update the index
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/secured-page':
+        return secured_layout
+    elif pathname == '/login':
+        return login_layout
+    else:
+        return index_page
+    # You could also return a 404 "URL not found" page here
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
